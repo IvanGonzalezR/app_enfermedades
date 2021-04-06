da199fc9e4ae54deed6fb9f1c6acbff7316e2acd
Jose angel <73683594+josearg150@users.noreply.github.com>
2021-04-05 23:29:37 -0500

    Cambios en la estructura, mayor parecido al diseño

--- lib/main.dart
+++ lib/main.dart
@@ -2,128 +2,41 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
-  runApp(MyApp());
-}
+//PAra utilizar variables y que guarden sus datos sera necesario crear un stateful widget
+void main() =>  runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent));
-
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'AppEnfermedades',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'App Enfermedades equipo 8'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent));
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "EQUIPO 8",
-              style: Theme.of(context).textTheme.headline2,
-            ),
-            Text(
-              "Noticias y articulos",
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text(
-              "Enfermedades",
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      title: 'Equipo 8',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('App Enfermedades'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(CupertinoIcons.question),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+            Text("EQUIPO 8",style: TextStyle(fontSize: 50, color: Colors.black)),
+              Text('Noticias y articulos',style: TextStyle(fontSize: 40, color: Colors.lightBlue)),
+              //en la parte de onPressed se agregara el metodo para ir a la siguiente pagina
+              new RaisedButton(color: Colors.blue,textColor: Colors.white,child: Text('Enterate de las ultimas noticias \nsobre la salud',
+                  style: TextStyle(fontSize: 20.0)),onPressed: (){}),
+              Text('Enfermedades',style: TextStyle(fontSize: 40, color: Colors.lightBlue)),
+              new RaisedButton(color: Colors.blue,textColor: Colors.white,child: Text('Obten informacion sobre una\n amplia cantidad de enfermedades',
+                  style: TextStyle(fontSize: 20.0)),onPressed: (){}),
+            ],
+          ),
+        ),
+        //Boton signo de interrogacion
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){},
+          tooltip: 'Informacion sobre nosotros',
+          child: Icon(CupertinoIcons.question),
+        ),
+      )
     );
   }
 }
