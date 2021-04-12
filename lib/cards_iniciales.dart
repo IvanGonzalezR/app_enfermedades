dff0ec579a547141aea6afc5fcf39d1e0f12a85f
Ivan Gonzalez <kikin-gr2000@hotmail.com>
2021-04-12 15:34:48 -0500

    pantalla principal terminada

---
+++ lib/cards_iniciales.dart
@@ -0,0 +1,102 @@
+import 'package:app_enfermedades/pantalla_enfermedades.dart';
+import 'package:app_enfermedades/pantalla_noticias_articulos.dart';
+import 'package:flutter/material.dart';
+
+class CardsIniciales extends StatefulWidget {
+  final String texto;
+  final String path_image;
+  final int index;
+
+  CardsIniciales(this.texto, this.path_image, this.index);
+  @override
+  _CardsInicialesState createState() => _CardsInicialesState();
+}
+
+class _CardsInicialesState extends State<CardsIniciales> {
+
+  String path_image = "assets/images/covidInicial.jpg";
+
+  @override
+  Widget build(BuildContext context) {
+
+    final nombre_descripcion = Flexible(child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Flexible(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(widget.texto,
+                overflow: TextOverflow.clip,
+                style: TextStyle(
+                  fontFamily: "Lato",
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                  color: Colors.white
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    )
+    );
+
+    return InkWell(
+      onTap: (){
+        if(widget.index == 1){
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) =>PantallaNoticiasyArticulos()));
+        }else{
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) =>PantallaEnfermedades()));
+        }
+      },
+      splashColor: Color(0xff81bbff),
+      child: Stack(
+        alignment: Alignment.bottomRight,
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height/5,
+            padding: EdgeInsets.all(10.0),
+            margin: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+                color: Color(0xff459AFF),
+                borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  offset: Offset(3,5),
+                  blurRadius: 15,
+                  spreadRadius: 2
+                ),
+                BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(-5,-5),
+                    blurRadius: 15,
+                    spreadRadius: 2
+                )
+              ]
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                nombre_descripcion,
+                Container(
+                  margin: EdgeInsets.only(left: 8.0),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(50.0)),
+                  child: Image(image: AssetImage(widget.path_image),
+                      // height: MediaQuery.of(context).size.width/4,
+                      width: MediaQuery.of(context).size.width/2,
+                      fit: BoxFit.fill),
+                  ),
+              ],
+            ),
+          ),
+          //FAB
+        ],
+      ),
+    );
+  }
+}
--- lib/pantalla_principal.dart
+++ lib/pantalla_principal.dart
@@ -1,3 +1,4 @@
+import 'package:app_enfermedades/cards_iniciales.dart';
 import 'package:app_enfermedades/pantalla_acerca_de.dart';
 import 'package:app_enfermedades/pantalla_enfermedades.dart';
 import 'package:app_enfermedades/pantalla_noticias_articulos.dart';
@@ -29,21 +30,36 @@ class PantallaPrincipal extends StatelessWidget {
       ),
     );
 
+
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('App Enfermedades'),
+      backgroundColor: const Color(0xffefefef),
+      body: CustomScrollView(
+        physics: ClampingScrollPhysics(),
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Color(0xff459AFF),
+            stretch: true,
+            elevation: 1.0,
+            forceElevated: true,
+            expandedHeight: MediaQuery.of(context).size.height/17 ,
+            centerTitle: true,
+            title: Text("Sicklopedia App",textAlign: TextAlign.center, style: TextStyle(
+                fontFamily: "Lato",
+                fontSize: 30.0,
+                fontWeight: FontWeight.w400,
+                color: Colors.white
+            ),),
+            // flexibleSpace:
+          ),
+          SliverList(delegate: SliverChildListDelegate([
+            Divider(height: MediaQuery.of(context).size.height/6, color: Colors.transparent,),
+            CardsIniciales("Enterate de las últimas noticias sobre Covid-19 y mucho más!!", "assets/images/covidInicial.jpg", 1),
+            Divider(height: MediaQuery.of(context).size.height/15, color: Colors.transparent),
+            CardsIniciales("Obten información sobre una amplia cantidad de enfermedades", "assets/images/enfermedadesInicial.jpg", 2),
+          ]))
+        ],
       ),
-      body: content,
-      //Boton signo de interrogacion
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          Navigator.of(context).push(CupertinoPageRoute(
-              builder: (context) => PantallaAcercaDe()));
-        },
-        tooltip: 'Información sobre nosotros',
-        child: Icon(CupertinoIcons.question),
-      ),
-    )
-    ;
+    );
   }
 }
