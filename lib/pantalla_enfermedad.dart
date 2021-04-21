59c214183c4352a339c964be9fdf0e2db46f2586
Ivan Gonzalez <kikin-gr2000@hotmail.com>
2021-04-20 23:14:56 -0500

    Jala a medias, falta info

--- lib/pantalla_enfermedad.dart
+++ lib/pantalla_enfermedad.dart
@@ -20,11 +20,11 @@ class PantallaEnfermedad extends StatefulWidget {
 class _PantallaEnfermedadState extends State<PantallaEnfermedad> {
   ScrollController _scrollController;
   List apartados =[
-    {'nombreApartado': '¿Qué es?', 'seleccionado': false, 'id': '1'},
-    {'nombreApartado': 'Estadísticas', 'seleccionado': false, 'id': '2'},
-    {'nombreApartado': 'Síntomas', 'seleccionado': false, 'id': '3'},
-    {'nombreApartado': 'Tratamiento', 'seleccionado': false, 'id': '4'},
-    {'nombreApartado': 'Prevención', 'seleccionado': false, 'id': '5'},
+    {'nombreApartado': '¿Qué es?', 'seleccionado': false, 'id': '0'},
+    {'nombreApartado': 'Estadísticas', 'seleccionado': false, 'id': '1'},
+    {'nombreApartado': 'Síntomas', 'seleccionado': false, 'id': '2'},
+    {'nombreApartado': 'Tratamiento', 'seleccionado': false, 'id': '3'},
+    {'nombreApartado': 'Prevención', 'seleccionado': false, 'id': '4'},
   ];
   var selectedSlide;
 
@@ -63,54 +63,82 @@ class _PantallaEnfermedadState extends State<PantallaEnfermedad> {
 
     var maxScrollValue = _scrollController.position.maxScrollExtent;
 
-    var divisor = (maxScrollValue / apartados.length) + 20;
+    var divisor = (maxScrollValue / apartados.length-1) + 20;
 
     var scrollToValue = whichSlide * divisor;
 
     _scrollController.animateTo(scrollToValue,
-        curve: Curves.easeIn, duration: Duration(milliseconds: 1000));
+        curve: Curves.easeIn, duration: Duration(milliseconds: 500));
+
   }
 
   // CONTENIDO DE LA PANTALLA
   Widget getCards(slide) {
-    return Container(
-      height: MediaQuery.of(context).size.height/10,
-      padding: EdgeInsets.only(left: 10.0),
-      margin: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height/35),
-      decoration: BoxDecoration(
-        color: Colors.white,
-          boxShadow: [
-            BoxShadow(color: Colors.grey[200], blurRadius: 15, spreadRadius: 8)
-          ],
-          border: Border.all(
-              color: Colors.grey.withOpacity(0.3),
-              style: BorderStyle.solid,
-              width: 1.0,
-          ),
-        ),
+    return Padding(
+      padding: EdgeInsets.only(top: 10.0),
+      // padding: EdgeInsets.only(top: 10.0),
+      // height: MediaQuery.of(context).size.height/10,
+      // padding: EdgeInsets.only(left: 10.0),
+      // margin: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height/35),
+      // decoration: BoxDecoration(
+      //   color: Colors.white,
+      //     boxShadow: [
+      //       BoxShadow(color: Colors.grey[200], blurRadius: 15, spreadRadius: 8)
+      //     ],
+      //     border: Border.all(
+      //         color: Colors.grey.withOpacity(0.3),
+      //         style: BorderStyle.solid,
+      //         width: 1.0,
+      //     ),
+      //   ),
         // width: 125.0,
-        child: Column(
-          children: [
-            Text(
-              slide['nombreApartado'],
-              style: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  fontSize: MediaQuery.of(context).size.width/20,
-                  color: Colors.black,
-                  fontFamily: 'Lato'
-              ),
-            ),
-            Text(
-              widget.datos[int.parse(slide['id'])-1],
-              style: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  fontSize: MediaQuery.of(context).size.width/29,
-                  color: Colors.black,
-                  fontFamily: 'Lato'
-              ),
-            ),
-          ],
+        child: Container(
+          padding: EdgeInsets.only(bottom: 20.0, left: MediaQuery.of(context).size.width/15, right: MediaQuery.of(context).size.width/15),
 
+          decoration: BoxDecoration(
+            color: Colors.white,
+              boxShadow: [
+                BoxShadow(color: Colors.grey[200], blurRadius: 15, spreadRadius: 8)
+              ],
+              border: Border.all(
+                  color: Colors.grey.withOpacity(0.3),
+                  style: BorderStyle.solid,
+                  width: 1.0,
+              ),
+            ),
+
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 10.0),
+                // margin: EdgeInsets.only(top: 10.0),
+                child: Text(
+                  slide['nombreApartado'],
+                  style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: MediaQuery.of(context).size.width/20,
+                      color: Colors.black,
+                      fontFamily: 'Lato'
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                margin: EdgeInsets.only(top: 10.0),
+                child: Text(
+                  widget.datos[int.parse(slide['id'])],
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: MediaQuery.of(context).size.width/29,
+                      color: Colors.black,
+                      fontFamily: 'Lato'
+                  ),
+                ),
+              ),
+            ],
+
+          ),
         ),
     );
   }
@@ -130,7 +158,7 @@ class _PantallaEnfermedadState extends State<PantallaEnfermedad> {
 
           padding: EdgeInsets.only( left: 8.0, right: 8.0),
           margin: EdgeInsets.only(right: MediaQuery.of(context).size.width/40, left: MediaQuery.of(context).size.width/40,
-          top: MediaQuery.of(context).size.height/80, bottom: MediaQuery.of(context).size.height/80),
+          top: MediaQuery.of(context).size.height/95, bottom: MediaQuery.of(context).size.height/95),
           
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(50.0),
@@ -175,7 +203,7 @@ class _PantallaEnfermedadState extends State<PantallaEnfermedad> {
       // color: Color(0xff459AFF),
       child: ListView(
         scrollDirection: Axis.horizontal,
-        physics: BouncingScrollPhysics(),
+        // physics: BouncingScrollPhysics(),
         children: apartados.map((e) {
           return getTitles(e);
         }).toList(),
@@ -186,6 +214,7 @@ class _PantallaEnfermedadState extends State<PantallaEnfermedad> {
     final cuerpo = Container(
       width: double.infinity,
       child: ListView(
+        padding: EdgeInsets.only(top: 0.0),
         shrinkWrap: true,
         physics: BouncingScrollPhysics(),
         controller: _scrollController,
--- lib/tarjetas_enfermedades.dart
+++ lib/tarjetas_enfermedades.dart
@@ -18,7 +18,12 @@ class _TarjetasEnfermedadesState extends State<TarjetasEnfermedades> {
       onTap: (){
         Navigator.of(context).push(CupertinoPageRoute(
             builder: (context) => PantallaEnfermedad(widget.nombreEnfermedad, widget.pathImage,
-            "quees","stats","sintomasss","tratamiento","prevencion",)));
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap i,"
+        ,"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap i"
+              , "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap i"
+              ,"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap i"
+              ,"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap i"
+              ,)));
       },
       child: Container(
         decoration: BoxDecoration(
